fa2306c63402f48a5af1241c410027342483e13e
archanac <archanac@PIPLMUMDT073>
2023-03-08 14:41:58 +0530

    commit

---
+++ SMART_TAX_API/Models/AUDIT_ISSUE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMART_TAX_API.Models
+{
+    public class AUDIT_ISSUE
+    {
+        public int ID { get; set; }
+
+        public String ISSUE { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? RAISED_DATE { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? DUE_DATE { get; set; }
+
+        public String STATUS { get; set; }
+
+        public String SEVERITY { get; set; }
+
+        public String RESOLUTION { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? CLOSURE_DATE { get; set; }
+
+
+    }
+}
